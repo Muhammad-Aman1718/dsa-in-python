1ef8dcbd8a04c37c95e148b476cc061443f5bd5d
Muhammad-Aman1718 <ag3734359@gmail.com>
2025-05-15 00:14:26 +0500

    Working

---
+++ DSA-Problems/hackerRank/simpleArraySum.py
@@ -0,0 +1,14 @@
+
+def simpleArraySum(ar):
+    a = 0
+    for num in ar:
+        a+= num
+    return a
+
+arr = [1,2,4,6,7,8]
+
+
+result = simpleArraySum(arr)
+
+print ("This is full plus "  , result)
+
