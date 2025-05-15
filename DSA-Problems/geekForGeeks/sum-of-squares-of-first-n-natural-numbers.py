12804ccc6e94d64305a16a5767b5829bbf1dc7ce
Muhammad-Aman1718 <ag3734359@gmail.com>
2025-05-15 21:42:37 +0500

    sum-of-squares-of-first-n-natural-numbers

--- DSA-Problems/geekForGeeks/sum-of-squares-of-first-n-natural-numbers.py
+++ DSA-Problems/geekForGeeks/sum-of-squares-of-first-n-natural-numbers.py
@@ -2,12 +2,12 @@
 
 def sumSquareFirstNaturalNumber(num):
     sumOFAllNumbers = 0
-    for n in range(1, num):
-        print(n)
+    for n in range(1, num +1):
+        sumOFAllNumbers +=  n ** 2
     return sumOFAllNumbers
 
 
 
-num  = +input("Enter your number : ")
+num  = int(input("Enter your number : "))
 
 print(sumSquareFirstNaturalNumber(num))
